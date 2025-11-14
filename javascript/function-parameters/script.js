ae611530944ccc5e6cb00e2db93d4816c19f21bf
MystCely <157691471+MystCely@users.noreply.github.com>
2025-11-14 13:47:21 +0100

    added function-parameters assignment

---
+++ javascript/function-parameters/script.js
@@ -0,0 +1,11 @@
+function changeBg(color, width, height) {
+    document.getElementById("mainDiv").innerHTML = /*html*/`
+    <div style="background-color: ${color}; width: ${width}px; height: ${height}px; border-radius: 10px;"></div>
+    `;
+}
+
+ function changeBg2(color,width,height) {
+    document.getElementById("mainDiv").innerHTML = /*html*/`
+    <div style="background-color: ${color}; width: ${width}px; height: ${height}px; border-radius: 10px;"></div>
+   `;
+}
